77a2227718ea566ef5e1b893ca24a927107caef3
Daineair <sces2859615s@gmail.com>
2023-03-01 15:04:35 +0800

    w03 crown2_28 route

--- routes/crown2_28.js
+++ routes/crown2_28.js
@@ -4,9 +4,19 @@ var router = express.Router();
 let db = require('../utils/database');
 
 router.get('/', async function(req, res, next){
-    let results = await db.query('select * from category_28')
-    console.log('category data', JSON.stringify(results.rows));
-    res.render('crown2_28/index', { title: 'XuEn Wu',ID:'410410228' })
+    try {
+        let results = await db.query(`select * from category_28`);
+        console.log('results', JSON.stringify(results.rows));
+        res.render('crown2_28/index', { 
+            data: results.rows,
+            title: 'XuEn Wu',
+            ID:'410410228' 
+        });
+    }catch(error){
+        console.log(error);
+    }
+    
+    
 });
 
 module.exports = router;
--- utils/database.js
+++ utils/database.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    port: '5432',
+    port: '5433',
     database: 'crown_28',
     password: '0000',
 });
 
-console.log('pool', pool.options.database);
+console.log(`Postgres server running on ${pool.options.database} database`);
 
 module.exports = pool;
