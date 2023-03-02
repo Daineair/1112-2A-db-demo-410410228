08102390f4b954a432c98b91d64cd700213dcea4
Daineair <sces2859615s@gmail.com>
2023-03-02 15:01:00 +0800

    w03 test finish crown2_28

--- utils/data/category_28.sql
+++ utils/data/category_28.sql
@@ -7,10 +7,10 @@ CREATE TABLE category_28(
     link_url varchar(255)
 )
 
-INSERT INTO category_28(id, name, size, local_url,remote_url, link_url)
+INSERT INTO category_28(id, name, size, remote_url , local_url, link_url)
  VALUES
- (1, 'hates', null, '/img/homepage/hates.png', 'https://i.ibb.co/cvpntL1/hats.png', '/crown2_28/shop_28/hats'),
- (2, 'jackets', null, '/img/homepage/jackets.png', '/crown2_28/shop_28/jackets', '/crown2_28/shop_28/jackets'),
+ (1, 'hats', null, '/img/homepage/hats.png', 'https://i.ibb.co/cvpntL1/hats.png', '/crown2_28/shop_28/hats'),
+ (2, 'jackets', null, '/img/homepage/jackets.png', 'https://i.ibb.co/px2tCc3/jackets.png', '/crown2_28/shop_28/jackets'),
  (3, 'sneakers', null, '/img/homepage/sneakers.png', 'https://i.ibb.co/0jqHpnp/sneakers.png', '/crown2_28/shop_28/sneakers'),
  (4, 'womens', 'large', '/img/homepage/womens.png', 'https://i.ibb.co/GCCdy8t/womens.png', '/crown2_28/shop_28/womens'),
  (5, 'mens', 'large', '/img/homepage/mens.png', 'https://i.ibb.co/R70vBrQ/men.png', '/crown2_28/shop_28/mens')
