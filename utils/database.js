7ed8b0fcc2c542ab5bcc201ef78b806c037f8466
Daineair <sces2859615s@gmail.com>
2023-03-23 01:34:02 +0800

    w06 p4 not 0323

--- utils/database.js
+++ utils/database.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    port: '5433',
-    database: 'crown_28',
-    password: '0000',
+    port: '5432',
+    database: 'crown2_28',
+    password: 'postgres',
 });
 
 console.log(`Postgres server running on ${pool.options.database} database`);
