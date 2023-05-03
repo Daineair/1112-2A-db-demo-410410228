d598c31dc365e60e5addc987b4be61310bdab884
Daineair <sces2859615s@gmail.com>
2023-05-03 15:01:46 +0800

    w12 0503

--- routes/crown2_28.js
+++ routes/crown2_28.js
@@ -5,8 +5,8 @@ let db = require('../utils/database');
 
 router.get('/', async function(req, res, next){
     try {
-        let results = await db.query('select * from category_28');
-        console.log('category data', JSON.stringify(results.rows));
+        let results = await db.query('select * from category2_28');
+        //console.log('category data', JSON.stringify(results.rows));
         res.render('crown2_28/index', { 
             data: results.rows,
             title: 'XuEn Wu',
@@ -17,6 +17,27 @@ router.get('/', async function(req, res, next){
     }
     
     
+});
+
+router.get('/shop2_28/:category', async function (req, res, next){
+    console.log('category', req.params.category);
+    try {
+        let results = await db.query(`
+            select C.name as category, S.id, S.name, price, S.local_url 
+            from category2_28 as C, shop2_28 as S
+            where S.cat_id = C.id and C.name = $1
+        `, [req.params.category]);
+        console.log('results', JSON.stringify(results.rows));
+        res.json(results.rows);
+        /*res.render('crown2_28/shop2_28',{
+            data:results.rows,
+            category: req.params.category;
+            title: 'XuEn Wu',
+            ID:'410410228',
+        })*/
+    }catch(error){
+        console.log(error);
+    }
 });
 
 module.exports = router;
--- utils/test-db.js
+++ utils/test-db.js
@@ -3,7 +3,11 @@ const db = require('./database');
 
 const testDB = async () => {
     try {
-        let results = await db.query(`select * from category_28`);
+        let results = await db.query(`
+            select C.name as category, S.id, S.name, price, S.local_url 
+            from category2_28 as C, shop2_28 as S
+            where S.cat_id = C.id and C.name = 'sneakers'
+        `);
         console.log('results', JSON.stringify(results.rows));
     }catch(error){
         console.log(error);
